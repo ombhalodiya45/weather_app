cea4fa21bb353b8c366729d3094743c3dcac4e76
om bhalodiya <ombhalodiya0@gmail.com>
2025-09-05 15:31:56 +0530

    fetch data from api and setup the result

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,24 +4,44 @@ import "./SearchBox.css"
 import { useState } from 'react';
 
 export default function SearchBox() {
-
     let [city, setCity] = useState("");
 
-    let handleChange = (evt)=>{
+    const API_URL = import.meta.env.VITE_API_URL;
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
+    let getWeatherInfo = async (city) => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+
+        let result = {
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempMax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+
+        console.log(result);
+    }
+
+    let handleChange = (evt) => {
         setCity(evt.target.value);
     };
 
-    let handleSubmit = (evt)=>{
+    let handleSubmit = (evt) => {
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo(city);
     }
 
     return (
         <div className='SearchBox'>
             <h3>Seach for the weather</h3>
             <form>
-                <TextField id="city" label="Search City" variant="outlined" required value={city} onChange={handleChange}/>
+                <TextField id="city" label="Search City" variant="outlined" required value={city} onChange={handleChange} />
                 <br></br>
                 <br></br>
                 <Button variant="contained" type='submit' onClick={handleSubmit}>Search</Button>
